f14662146a963909fc4a2d5d905892fc79d1e8da
Muhammad Nasir <muhammadnasir512@gmail.com>
2022-03-21 00:10:17 +0000

    Initial Commit

---
+++ Shopping/ShoppingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingApp.swift
+//  Shopping
+//
+//  Created by Muhammad Nasir Ali on 21/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
